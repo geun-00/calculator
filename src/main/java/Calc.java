a1b64cbb73415d28ef4e53e58e94cbd708aa0f27
GeunYeong <jgy091411@gmail.com>
2025-08-02 09:00:09 +0900

    :recycle: 코드 리팩토링 - 공통적인 부분 메서드화 - 예외 처리 추가

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -4,37 +4,19 @@ public class Calc {
         return calculate(expression.replaceAll(" ", "").trim());
     }
 
-    //기저 조건 = 식이 아닌 숫자가 단 하나
     private static double calculate(String expr) {
-
         if (expr.startsWith("(") && getMatchingBracketIndex(expr) == expr.length() - 1) {
             return calculate(expr.substring(1, expr.length() - 1));
         }
 
-        int pmIndex = solve(expr, '+', '-');
+        int pmIndex = getOperatorIndex(expr, '+', '-');
         if (pmIndex != -1) {
-            char oper = expr.charAt(pmIndex);
-            String left = expr.substring(0, pmIndex);
-            String right = expr.substring(pmIndex + 1);
-
-            double leftResult = calculate(left);
-            double rightResult = calculate(right);
-
-            if (oper == '+') return leftResult + rightResult;
-            else return leftResult - rightResult;
+            return divideAndCalculate(expr, pmIndex);
         }
 
-        int mdIndex = solve(expr, '*', '/');
+        int mdIndex = getOperatorIndex(expr, '*', '/');
         if (mdIndex != -1) {
-            char oper = expr.charAt(mdIndex);
-            String left = expr.substring(0, mdIndex);
-            String right = expr.substring(mdIndex + 1);
-
-            double leftResult = calculate(left);
-            double rightResult = calculate(right);
-
-            if (oper == '*') return leftResult * rightResult;
-            else return leftResult / rightResult;
+            return divideAndCalculate(expr, mdIndex);
         }
 
         if (expr.startsWith("-")) {
@@ -44,7 +26,33 @@ public class Calc {
         return Double.parseDouble(expr);
     }
 
-    private static int solve(String expr, char op1, char op2) {
+    private static double divideAndCalculate(String expr, int pmIndex) {
+        char oper = expr.charAt(pmIndex);
+        String left = expr.substring(0, pmIndex);
+        String right = expr.substring(pmIndex + 1);
+
+        return calculate(left, oper, right);
+    }
+
+    private static double calculate(String left, char oper, String right) {
+        double leftResult = calculate(left);
+        double rightResult = calculate(right);
+
+        return switch (oper) {
+            case '+' -> leftResult + rightResult;
+            case '-' -> leftResult - rightResult;
+            case '*' -> leftResult * rightResult;
+            case '/' -> {
+                if (rightResult == 0.0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+                yield leftResult / rightResult;
+            }
+            default -> throw new IllegalStateException("지원하지 않는 연산자입니다 : " + oper);
+        };
+    }
+
+    private static int getOperatorIndex(String expr, char op1, char op2) {
         int count = 0;
 
         for (int i = expr.length() - 1; i >= 0; i--) {
@@ -72,6 +80,7 @@ public class Calc {
 
     private static int getMatchingBracketIndex(String expr) {
         int count = 0;
+
         for (int index = 0; index < expr.length(); index++) {
             char ch = expr.charAt(index);
 
